af7ee206c69cfb99c68a08513199f408eee83076
Tanner Pack <tannerpack@Tanners-MacBook-Air.local>
2025-03-05 19:01:39 -0600

    Finished MiddleEarthCouncil.java

--- src/middleearth/mu/edu/management/MiddleEarthCouncil.java
+++ src/middleearth/mu/edu/management/MiddleEarthCouncil.java
@@ -1,5 +1,20 @@
 package middleearth.mu.edu.management;
 
 public class MiddleEarthCouncil {
-
-}
+    private static MiddleEarthCouncil instance;  // Single instance
+    private CharacterManager characterManager;   // Manages characters    
+    private MiddleEarthCouncil() {
+    	characterManager = new CharacterManager(); //Initialize CharacterManager
+    }
+    
+    public static MiddleEarthCouncil getInstance() {
+    	if (instance == null) {
+    		instance = new MiddleEarthCouncil();
+    	}
+    	return instance;
+    }
+    
+    public CharacterManager getCharacterManager() {
+    	return characterManager;
+    }
+} 
