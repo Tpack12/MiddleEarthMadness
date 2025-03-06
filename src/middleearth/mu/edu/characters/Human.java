cfcd7d61fd880f37b21fa7a4c23e1047bd57b123
Jacob Stirnemann <jacobstirnemann@Jacobs-MacBook-Pro.local>
2025-03-05 18:49:25 -0600

    Created Human.java class

--- src/middleearth/mu/edu/characters/Human.java
+++ src/middleearth/mu/edu/characters/Human.java
@@ -1,5 +1,28 @@
 package middleearth.mu.edu.characters;
 
-public class Human {
-
+//Constructor initializes Human character details
+class Human extends MiddleEarthCharacter {
+	public Human(String name, double health, double power) {
+		super(name, health, power);
+	}
+	
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		if (target.getRace().equals("Human") || target.getRace().equals("Orc")) {
+			return false; //Does no damage towards Human and Orc
+		}
+		
+		double damage = this.power;
+		if (target.getRace().equals("Wizard")) {
+			damage *= 1.5; //Does extra damage towards Wizard
+		}
+		target.health -= damage;
+		return true; //Attack was successful
+	}
+	
+	//Returns the race of the character
+	@Override
+	public String getRace(){
+		return "Human";
+	}
 }
