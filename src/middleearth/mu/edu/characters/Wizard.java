c25dde885c2796a3073f45e53e8c6ee5ff273e51
Jacob Stirnemann <jacobstirnemann@Jacobs-MacBook-Pro.local>
2025-03-05 18:55:44 -0600

    Created Wizard.java subclass. Completed MiddleEarthCharacter abstract class.

--- src/middleearth/mu/edu/characters/Wizard.java
+++ src/middleearth/mu/edu/characters/Wizard.java
@@ -1,5 +1,28 @@
 package middleearth.mu.edu.characters;
 
-public class Wizard {
-
+//Constructor initializes Wizard character details
+class Wizard extends MiddleEarthCharacter {
+	public Wizard(String name, double health, double power) {
+		super(name, health, power);
+	}
+	
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		if (target.getRace().equals("Wizard") || target.getRace().equals("Human")) {
+			return false; //Does no damage towards Wizard and Human
+		}
+		
+		double damage = this.power;
+		if (target.getRace().equals("Dwarf")) {
+			damage *= 1.5; //Does extra damage towards Dwarf
+		}
+		target.health -= damage;
+		return true; //Attack was successful
+	}
+	
+	//Returns the race of the character
+	@Override
+	public String getRace(){
+		return "Wizard";
+	}
 }
