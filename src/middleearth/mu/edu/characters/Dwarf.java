c19e3efdbdf8f57527b3d90dc7bbd23c5125d197
Jacob Stirnemann <jacobstirnemann@Jacobs-MacBook-Pro.local>
2025-03-05 18:06:50 -0600

    Created Dwarf.java Class

--- src/middleearth/mu/edu/characters/Dwarf.java
+++ src/middleearth/mu/edu/characters/Dwarf.java
@@ -1,5 +1,29 @@
 package middleearth.mu.edu.characters;
 
-public class Dwarf{
+//Constructor initializes Dwarf character details
+class Dwarf extends MiddleEarthCharacter {
+	public Dwarf(String name, double health, double power) {
+		super(name, health, power);
+	}
 	
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		if (target.getRace().equals("Dwarf") || target.getRace().equals("Wizard")) {
+			return false; //Does no damage towards Wizard
+		}
+		
+		double damage = this.power;
+		if (target.getRace().equals("Elf")) {
+			damage *= 1.5; //Does extra damage towards Elf
+		}
+		target.health -= damage;
+		return true; //Attack was successful
+	}
+	
+	//Returns the race of the character
+	@Override
+	public String getRace() {
+		return "Dwarf";
+	}
 }
+
