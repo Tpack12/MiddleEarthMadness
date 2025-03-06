2bfebdc75964814f0ebb53f77f2a2a53a451c339
Tanner Pack <tannerpack@Tanners-MacBook-Air.local>
2025-03-05 18:22:37 -0600

    Started working on CharacterManager. So far I have added a couple constructors as well as that it will use a dynamically resizeable array.

--- src/middleearth/mu/edu/management/CharacterManager.java
+++ src/middleearth/mu/edu/management/CharacterManager.java
@@ -1,5 +1,31 @@
 package middleearth.mu.edu.management;
 
-public class CharacterManager {
+import middleearth.mu.edu.character.MiddleEarthCharacter;
 
+public class CharacterManager {
+	private middleearth.mu.edu.characters.MiddleEarthCharacter[] characters;
+	private int size;
+	
+	public CharacterManager() {
+		characters = new MiddleEarthCharacter[5];
+		size = 0;
+	}
+	
+	public boolean addCharacter(MiddleEarthCharacter c) {
+		if (size >= characters.length) {
+			resizeArray();
+		}
+		characters[size++] = c;
+		return true;
+	}
+	
+	public MiddleEarthCharacter getCharacter(String name) {
+		for (int i = 0; i < size; i++) {
+			if (characters[i].getName().equalsIgnoreCase(name)) {
+				return characters[i];
+			}
+		}
+		return null;
+	}
+	
 }
